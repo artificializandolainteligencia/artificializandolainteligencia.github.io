56f73a42f731aea6d7c3d3f32ba7ef0d4c1f9da6
faturita <rramele@gmail.com>
2025-11-12 21:17:36 +0000

    Deploying to gh-pages from @ artificializandolainteligencia/artificializandolainteligencia.github.io@ef3c8871739d5d2ddeb54a0b447da35eae132aad 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -3,28 +3,21 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-about",
-    title: "about",
+    id: "nav-libro",
+    title: "libro",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-cv",
-          title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/cv/";
-          },
-        },{id: "nav-teaching",
+  },{id: "nav-teaching",
           title: "teaching",
           description: "Materials for courses you taught. Replace this text with your description.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/teaching/";
           },
-        },{id: "nav-people",
-          title: "people",
+        },{id: "nav-autores",
+          title: "autores",
           description: "members of the lab or group",
           section: "Navigation",
           handler: () => {
